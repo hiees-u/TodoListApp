509863d7cce0ebe07f80836beeab602618575d0c
hiuuuu <hieu.nguyenminh@hdwebsoft.dev>
2025-08-01 10:18:37 +0700

    fix cicd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import * as path from 'path'
 
 export default defineConfig({
   base: '/TodoListApp/', // <- tên repo
